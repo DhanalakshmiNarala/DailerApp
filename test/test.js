604a4c07cba49a464e4294e3c83ed9141675a800
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-22 13:11:04 +0530

    Code refactoring

--- test/test.js
+++ test/test.js
@@ -1,39 +1,39 @@
 const assert = require('chai').assert
 const dialerApp = require('../dialerApp')
 
-describe('Dialer dialerApp', function() {
-    it('Keanu Reeves - 96857 68789', function() {
-        let contactName = "Keanu Reeves"
-        dialerApp.insertContact(contactName, "96857 68789")
-        assert.equal(contactName, dialerApp.search("53268"))
-        assert.equal(contactName, dialerApp.search("733837"))
-        assert.equal(contactName, dialerApp.search("96857"))
-        assert.equal(contactName, dialerApp.search("68789"))
+describe('Dialer dialerApp', ()=> {
+    it('Keanu Reeves - 96857 68789', ()=> {
+        let contactName = "Keanu Reeves", phoneNumber = "96857 68789"
+        dialerApp.insertContact(contactName, phoneNumber)
+        assert.equal(contactName, dialerApp.search("53268")) // Keanu - dialer pad code
+        assert.equal(contactName, dialerApp.search("733837")) // Reeves
+        assert.equal(contactName, dialerApp.search(phoneNumber.substring(0, 5)))
+        assert.equal(contactName, dialerApp.search(phoneNumber.substring(6, 11))) 
     })
 
-    it('Brad Pitt - 96877 83421', function() {
-        let contactName = "Brad Pitt"
-        dialerApp.insertContact(contactName, "96877 83421")
-        assert.equal(contactName, dialerApp.search("2723"))
-        assert.equal(contactName, dialerApp.search("7488"))
-        assert.equal(contactName, dialerApp.search("96877"))
-        assert.equal(contactName, dialerApp.search("83421"))
+    it('Brad Pitt - 96877 83421', ()=> {
+        let contactName = "Brad Pitt", phoneNumber = "96877 83421"
+        dialerApp.insertContact(contactName, phoneNumber)
+        assert.equal(contactName, dialerApp.search("2723")) // Brad
+        assert.equal(contactName, dialerApp.search("7488")) // Pitt
+        assert.equal(contactName, dialerApp.search(phoneNumber.substring(0, 5)))
+        assert.equal(contactName, dialerApp.search(phoneNumber.substring(6, 11)))
     })
 
-    it('Trisha - 97432 74880', function() {
-        let contactName = "Trisha"
-        dialerApp.insertContact(contactName, "97432 74880")
-        assert.equal(contactName, dialerApp.search("874742"))
-        assert.equal(contactName, dialerApp.search("97432"))
-        assert.equal(contactName, dialerApp.search("74880"))
+    it('Trisha - 97432 74880', ()=> {
+        let contactName = "Trisha", phoneNumber = "97432 74880"
+        dialerApp.insertContact(contactName, phoneNumber)
+        assert.equal(contactName, dialerApp.search("874742")) // Trisha
+        assert.equal(contactName, dialerApp.search(phoneNumber.substring(0, 5)))
+        assert.equal(contactName, dialerApp.search(phoneNumber.substring(6, 11)))
     })
 
-    it('Vijay Sethupathy - 99490 83597', function() {
-        let contactName = "Vijay Sethupathy"
-        dialerApp.insertContact(contactName, "99490 83597")
-        assert.equal(contactName, dialerApp.search("84529"))
-        assert.equal(contactName, dialerApp.search("7384872849"))
-        assert.equal(contactName, dialerApp.search("99490"))
-        assert.equal(contactName, dialerApp.search("83597"))
+    it('Vijay Sethupathy - 99490 83597', ()=> {
+        let contactName = "Vijay Sethupathy", phoneNumber = "99490 83597"
+        dialerApp.insertContact(contactName, phoneNumber)
+        assert.equal(contactName, dialerApp.search("84529")) // Vijay
+        assert.equal(contactName, dialerApp.search("7384872849")) // Sethupathy
+        assert.equal(contactName, dialerApp.search(phoneNumber.substring(0, 5)))
+        assert.equal(contactName, dialerApp.search(phoneNumber.substring(6, 11)))
     })
 })
